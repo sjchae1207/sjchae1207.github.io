e2e242eefc4e00c77491c5d4820e3c04ef9838f4
sjchae1207 <sjchea@kaist.ac.kr>
2021-05-21 01:40:30 +0900

    first commit

---
+++ content/blog/category1/2021-02-26---Research-interest.md
@@ -0,0 +1,18 @@
+---
+title: Presentation about Transfer Entropy
+date: "2021-05-21"
+draft: false
+slug: "research-interest"
+category: "Presentation"
+tags:
+	- Presentation
+    - Transfer Entropy
+description: "Extending Transfer Entropy Improves Identification of Effective Connectivity in a Spiking Cortical Network Model"
+---
+
+On May 14, I presented a paper  "Extending Transfer Entropy Improves Identification of Effective Connectivity in a Spiking Cortical Network Model"
+
+@ IBS journal club
+
+you can find the info [here](https://www.ibs.re.kr/bimag/event/2021-05-14/).
+
---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Seokjoo Chae`,
+  description: `Mathematical biology `,
+  author: `Seokjoo`,
+  introduction: `Ph.D. candidate in Math Bio @ KAIST`,
+  siteUrl: `https://sjchae1207.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `sjchae1207`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'sjchae1207/sjchae1207.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
